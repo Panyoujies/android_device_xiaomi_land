42b7726b51620b526dcf8f5149515976ba2e0da8
Anik1199 <anik9280@gmail.com>
2016-08-19 07:55:21 +0600

    ido: init: Set dalvik properties dynamically * Picked from kenzo * Nuked ro.alarm_boot prop as not working yet * ro.alarm_boot prop will be added later with single commit

--- device.mk
+++ device.mk
@@ -17,8 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-hwui-memory.mk)
 
 $(call inherit-product-if-exists, vendor/xiaomi/land/land-vendor.mk)
 $(call inherit-product-if-exists, device/xiaomi/land/vendor/copyfiles.mk)
